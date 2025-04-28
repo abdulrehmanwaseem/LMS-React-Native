0dad8a13a236b7f9785b53850cbe4866fb894d6c
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-04-28 19:29:26 +0500

    Add theme context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { Stack } from "expo-router";
 import React from "react";
 
 export default function RootLayout() {
+  const { Screen } = Stack;
+
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Screen name="index" />
+    </Stack>
   );
 }
