b53836f2f6b3f5043924f7c752b7470231d3b007
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-05-01 17:06:24 +0500

    Add onboarding screen, constants and types

--- app/(routes)/onboarding/index.tsx
+++ app/(routes)/onboarding/index.tsx
@@ -1,10 +1,6 @@
-import { View, Text } from "react-native";
+import OnboardingScreen from "@/screens/onboarding/onboardingScreen";
 import React from "react";
 
 export default function index() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 20, backgroundColor: "red" }}>Index</Text>
-    </View>
-  );
+  return <OnboardingScreen />;
 }
