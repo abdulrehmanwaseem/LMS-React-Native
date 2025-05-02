1c192df7410401d705a1a6107c311111d8f9b642
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-05-02 13:18:05 +0500

    Add google fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,26 @@
 import { ThemeProvider } from "@/context/theme.context";
 import { Stack } from "expo-router";
 import React from "react";
+import {
+  Poppins_600SemiBold,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_700Bold,
+  Poppins_500Medium,
+  useFonts,
+} from "@expo-google-fonts/poppins";
 
 export default function RootLayout() {
   const { Screen } = Stack;
+
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Poppins_600SemiBold,
+    Poppins_300Light,
+    Poppins_700Bold,
+    Poppins_400Regular,
+    Poppins_500Medium,
+  });
 
   return (
     <ThemeProvider>
