25db9217b15fd888c76e86db94943471acb5c057
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-05-06 17:30:50 +0500

    Add ts support to the server

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
