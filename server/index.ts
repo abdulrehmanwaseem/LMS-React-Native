0f034fd8fde90705a3f5c2a3172faeb22b626bb8
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-05-11 13:01:48 +0500

    Check for the signedToken from mobile app

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import "dotenv/config";
-import jwt from "jsnowebtoken";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const port = 3000;
@@ -9,6 +9,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 app.post("/login", async (req: Request, res: Response) => {
+  const { signedToken } = req.body;
+  const data = jwt.verify(signedToken, process.env.JWT_SECRET_KEY);
+  if (data) {
+    const isUserExits = null;
+  }
   res.json({
     status: 200,
   });
